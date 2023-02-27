cc64dba7a1989e33d0f4b968c2703eeb035912e5
Rhothomir <altindasahmet2@gmail.com>
2023-02-27 16:41:28 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import java.util.Scanner; // planetWeight
+public class MarsWeightCalc {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        float earthWeight;
+        float marsWeight;
+        double marsWeightInDouble;
+        int marsWeightInInt;
+
+        earthWeight = 64;
+        marsWeight = (earthWeight * 0.38F);
+        System.out.println(earthWeight + "kg on Earth is" + marsWeight);
+
+        System.out.
+
+
+
+
+
+
+
+
     }
 }
